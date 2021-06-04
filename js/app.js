a16ce4b174ac9a6953520d01f387a3a181ede120
JJmendoti <jmendoti.cesde@gmail.com>
2021-06-04 18:52:06 -0500

    Se hace cambios en el index 'Carousel'

--- js/app.js
+++ js/app.js
@@ -3,34 +3,31 @@ window.addEventListener('load', function () {
     $('#onload').fadeOut();
     $('body').removeClass('hidden');
 });
-
 //End Preloader
-
-
 
 //Carousel
 $(document).ready(function () {
     $("#carousel1").owlCarousel({
         loop: true,
         margin: 5,
-        autoHeight:true,
+        autoHeight: false,
         autoplay: true,
-        autoplayTimeout: 3000,
+        autoplayTimeout: 1000,
         autoplayHoverPause: true,
         responsiveClass: true,
         responsive: {
             0: {
-                items: 1,
+                items: 1
             },
             600: {
-                items: 1,
-                
+                items: 1
+
             },
             1000: {
-                items: 1
+                items: 1                
             }
         }
     });
+
 });
-
-
+//End  Carousel
